eb48dc27045b18824dc9af3097aaa1d538223dff
Damian <damianmirek1234@gmail.com>
2023-08-31 19:31:53 +0200

    Category model and controller removal

--- src/routes/categoryRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {
-  getAllCategories,
-  createCategory,
-} from "../controllers/categoryController";
-
-export const router = express.Router();
-
-router.route("/").get(getAllCategories).post(createCategory);
