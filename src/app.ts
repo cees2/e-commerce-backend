2ea4dfb19b35434dc551df32e4b6dce60a1d8975
Damian <damianmirek1234@gmail.com>
2023-09-02 15:03:53 +0200

    Create appError class & applied it first time

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import { router as productRouter } from "./routes/productRoutes";
 import { app } from "../server";
+import { AppError } from "./utls/AppError";
 
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use("/ajax/products", productRouter);
+
+app.all("*", (request, response, next) => {
+  next(new AppError(`Cannot find ${request.originalUrl} on this server`, 404));
+});
---
+++ src/utls/AppError.ts
@@ -0,0 +1,15 @@
+export class AppError extends Error {
+  private message: string;
+  private statusCode: number;
+  private status: string;
+  private isOperational: boolean;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this.this.constructor);
+  }
+}
