693ca5a50205016157a8ac9982794def72e8881f
Damian <damianmirek1234@gmail.com>
2023-09-11 19:04:09 +0200

    Protected routes done

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { signup, login } from "../controllers/user/authController";
-import { getUser } from "../controllers/user/userController";
+import { getUser, protect } from "../controllers/user/userController";
 
 export const router = express.Router();
 
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 
+router.use(protect);
 router.route("/:userId").get(getUser);
