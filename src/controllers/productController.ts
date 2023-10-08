5769aa027804c4754bcc8c1f11806ebed97e2007
Damian <damianmirek1234@gmail.com>
2023-10-08 21:38:54 +0200

    Added possibility to receive image file and store it in file system

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,6 +1,40 @@
 import { Product } from "../models/productModel";
 import { AppError } from "../utils/AppError";
 import { Request, Response, NextFunction } from "express";
+import multer from "multer";
+import { Express } from "express";
+
+const multerStorage = multer.diskStorage({
+  destination: (request, file, cb) => {
+    cb(null, "public/img/products");
+  },
+  filename: (request, file, cb) => {
+    const extension = file.mimetype.split("/")[1];
+    cb(null, `product-${request.user.id}-${Date.now()}.${extension}`);
+  },
+});
+
+const multerFilter = (
+  request: Request,
+  file: Express.Multer.File,
+  cb: (error: any, info: boolean) => void
+) => {
+  if (file.mimetype.startsWith("image")) cb(null, true);
+  else {
+    const error = new AppError(
+      "File is not an image. Please select correct file and upload again",
+      400
+    );
+    cb(error, false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+export const uploadProductPhoto = upload.single("image");
 
 export const createProduct = async (
   request: Request,
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -4,7 +4,11 @@ import {
   createProduct,
 } from "../controllers/productController";
 import { protect } from "../controllers/user/authController";
+import { uploadProductPhoto } from "../controllers/productController";
 
 export const router = express.Router();
 
-router.route("/").get(getAllProducts).post(protect, createProduct);
+router
+  .route("/")
+  .get(getAllProducts)
+  .post(protect, uploadProductPhoto, createProduct);
